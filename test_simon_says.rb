f2d670aa1adc0102d47cb8fa1e8961523799dbb1
Marlon O'Neil <hello@marlononeil.com>
2017-09-26 15:16:03 -0400

    skipped all tests except first

--- test_simon_says.rb
+++ test_simon_says.rb
@@ -8,30 +8,37 @@ class TestSimonSays < MiniTest::Test
   end
 
   def test_echo_bye
+    skip
     assert_equal "bye", echo("bye")
   end
 
   def test_shout_hello
+    skip
     assert_equal "HELLO", shout("hello")
   end
 
   def test_shout_multiple_words
+    skip
     assert_equal "HELLO WORLD", shout("hello world")
   end
 
   def test_repeat
+    skip
     assert_equal "hello hello hello", repeat("hello", 3)
   end
 
   def test_start_of_word_returns_first_letter
+    skip
     assert_equal "h", start_of_word("hello", 1)
   end
 
   def test_start_of_word_returns_first_two_letters
+    skip
     assert_equal "Bo", start_of_word("Bob", 2)
   end
-  
+
   def test_first_word
+    skip
     assert_equal "Hello", first_word("Hello World")
   end
 
